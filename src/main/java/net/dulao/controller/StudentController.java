f59316e673507a479f0b18dc235c0db5d1a9af72
tokke <602979469@qq.com>
2020-07-23 19:01:29 +0800

    第四次提交
    主要内容：
    添加了文档，并使用postman测试了所有的api接口

--- src/main/java/net/dulao/controller/StudentController.java
+++ src/main/java/net/dulao/controller/StudentController.java
@@ -1,31 +1,21 @@
 package net.dulao.controller;
 
 import net.dulao.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 /**
- * (Student)表控制层
+ * 学生控制器
  *
- * @author makejava
- * @since 2020-07-21 17:15:31
+ * @author Faker
+ * @date 2020/07/23
  */
 @RestController
-@RequestMapping("studentHandler")
+@RequestMapping("student")
 public class StudentController {
-    /**
-     * 服务对象
-     */
     @Resource
     private StudentService studentService;
 
-    @GetMapping("login")
-    public String login(String username,String password){
-        if(studentService.queryByUsername(username).get(0).getSPassword().equals(password))
-            return "success";
-        return "fail";
-    }
 }
